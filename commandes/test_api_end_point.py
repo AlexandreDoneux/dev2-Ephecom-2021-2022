f3e0befba1f94f4f0a57cc3dca9c37e5033a74c8
Kouvahe81 <93136077+Kouvahe81@users.noreply.github.com>
2021-12-12 14:05:22 +0100

    nouvelle version

---
+++ commandes/test_api_end_point.py
@@ -0,0 +1,9 @@
+import unittest
+from unittest import TestCase
+
+
+class TestItinerary(TestCase):
+    def test_show_itinerary(self):
+        self.assertTrue(" ")
+if __name__ == '__main__':
+    unittest.main()
