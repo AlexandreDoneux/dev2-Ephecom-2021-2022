280052aec13105e7a33bd0015daf55e693837025
Kouvahe81 <93136077+Kouvahe81@users.noreply.github.com>
2021-12-12 19:47:48 +0100

    complétion test unitaire

--- commandes/test_api_end_point.py
+++ commandes/test_api_end_point.py
@@ -1,9 +1,9 @@
 import unittest
-from unittest import TestCase
+from commandes import api_end_point
 
 
-class TestItinerary(TestCase):
+class TestItinerary(unittest.TestCase):
     def test_show_itinerary(self):
-        self.assertTrue(" ")
+        self.assertTrue(api_end_point.Itinerary().show_itinerary())
 if __name__ == '__main__':
     unittest.main()
