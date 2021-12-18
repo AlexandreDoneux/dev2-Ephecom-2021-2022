fdad6000f3d8220969fd9e0a93445d2a89e89a74
Kevin Keurvels <k.keurvels@students.ephec.be>
2021-12-18 15:13:31 +0100

    finalisation du fichier EpheCom

--- EpheCom.py
+++ EpheCom.py
@@ -1,18 +1,21 @@
 # Python 3.10
 # UTF -8
+import os
 import platform
 import subprocess
 if __name__ == "__main__":
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    script_path += "\\main.py"
     running = True
     while running:
-        attribut = input("Entrez une commande : (-1 pour quitter")
+        attribut = input("Entrez une commande (-1 pour quitter) : ")
         if attribut == '-1':
             running = False
         else:
             # va regarder quel est l'os pour savoir pour les droits admin
             parameter = '-n' if platform.system().lower() == 'windows' else '-c'
             # execute la commande
-            command = ['python3.10', 'main.py', '-chatbot', attribut]
+            command = ['python3', script_path, '-chatbot', attribut]
             response = subprocess.call(command)
             if response != 0:
                 print("Il y a un problème avec subprocess")
